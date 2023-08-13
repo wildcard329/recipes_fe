9b25d420d9e25758b6a3a9a534fd1c60b6f93a5d
wildcard329 <enkiduwildman@gmail.com>
2023-08-13 16:22:06 -0700

    [users-003] added new custom hooks and changed username to email on login

--- src/state/providers/RecipeProvider.jsx
+++ src/state/providers/RecipeProvider.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { recipeContext } from "../contexts";
+import { useArray } from "../../utils/customhooks";
 
 const RecipeProvider = ({ children }) => {
   const [recipe, setRecipe] = useState({});
+  const recipes = useArray();
 
   return(
-    <recipeContext.Provider value={{ recipe, setRecipe }}>
+    <recipeContext.Provider value={{ recipes, recipe, setRecipe }}>
       {children}
     </recipeContext.Provider>
   )
--- src/utils/customhooks/index.js
+++ src/utils/customhooks/index.js
@@ -1,2 +1,3 @@
+export { default as useArray } from './useArray.js';
 export { default as useBool } from "./useBool.js";
 export { default as useReactRouter } from "./useReactRouter.js";
