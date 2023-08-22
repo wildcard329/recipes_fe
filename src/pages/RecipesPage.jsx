bfe1c332a75a22ecfe2c4c85c3eeb814df86fc2a
wildcard329 <enkiduwildman@gmail.com>
2023-08-22 07:47:18 -0700

    [rec-005] corrected index.html

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API } from "aws-amplify";
 
-const RecipesPage = () => 
-  <p>RecipesPage works!</p>
+const RecipesPage = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = async () => {
+    API.get('recipes', '/recipes')
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return (
+    <p>Recipes works!</p>
+  )
+}
 
 export default RecipesPage;
