23a1cd70c32736295cfe56b81c4374f8c3f708ae
wildcard329 <enkiduwildman@gmail.com>
2023-08-23 12:54:14 -0700

    [rec-007] corrected AWS permissions issue

--- amplify/backend/function/reclambda/src/app.js
+++ amplify/backend/function/reclambda/src/app.js
@@ -1,4 +1,7 @@
-/*
+/* Amplify Params - DO NOT EDIT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT *//*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
@@ -12,7 +15,8 @@ See the License for the specific language governing permissions and limitations 
 const express = require('express')
 const bodyParser = require('body-parser')
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-const recipes = require('./recipes.json');
+const { getRecipes, getRecipeById, addOrUpdateRecipe, deleteRecipe } = require('./handlers/rec-handler.js');
+// const recipes = require('./recipes.json');
 
 // declare a new express app
 const app = express()
@@ -26,61 +30,73 @@ app.use(function(req, res, next) {
   next()
 });
 
-
 /**********************
  * Example get method *
  **********************/
 
-app.get('/recipes', function(req, res) {
-  res.json({ success: 'get call succeed!', recipes });
+app.get('/recipes', async (req, res) => {
+  try {
+    const recipes = await getRecipes();
+    res.json({ success: 'get call succeed!', recipes });
+  } catch (error) {
+    res.json({ error: 'error retrieving get call' });
+  };
 });
 
-app.get('/recipes/:recipeId', function(req, res) {
+app.get('/recipes/:recipeId', async (req, res) => {
   const recipeId = parseInt(req.params.recipeId);
-  const recipe = recipes.find((recipeItem) => recipeItem.recipe_id === recipeId);
-  res.json({ success: 'get call succeed!', recipe });
+  // const recipe = recipes.find((recipeItem) => recipeItem.recipe_id === recipeId);
+  try {
+    const recipe = await getRecipeById(recipeId);
+    res.json({ success: 'get call succeed!', recipe });
+  } catch (error) {
+    res.json({ error: 'error retrieving get call' });
+  }
 });
 
 /****************************
 * Example post method *
 ****************************/
 
-app.post('/recipes', function(req, res) {
+app.post('/recipes', async (req, res) => {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-app.post('/recipes/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  const recipe = req.body;
+  try {
+    await addOrUpdateRecipe(recipe);
+    res.json({success: 'post call succeed!' });
+  } catch (error) {
+    res.json({ error: 'error processing data' });
+  };
 });
 
 /****************************
 * Example put method *
 ****************************/
 
-app.put('/recipes', function(req, res) {
+app.put('/recipes', async (req, res) => {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-app.put('/recipes/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  const recipe = req.body;
+  try {
+    await addOrUpdateRecipe(recipe);
+    res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  } catch (error) {
+    res.json({ error: 'error processing request' });
+  };
 });
 
 /****************************
 * Example delete method *
 ****************************/
 
-app.delete('/recipes', function(req, res) {
+app.delete('/recipes/:recipeId', async (req, res) => {
   // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/recipes/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
+  const recipeId = req.params.recipeId;
+  try {
+    await deleteRecipe(recipeId);
+    res.json({success: 'delete call succeed!'});
+  } catch (error) {
+    res.json({ error: 'could not delete recipe' });
+  };
 });
 
 app.listen(3000, function() {
