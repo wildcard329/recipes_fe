67afbb05622d039fe2ee20ffe52e6941c11022cb
wildcard329 <enkiduwildman@gmail.com>
2023-08-24 19:47:35 -0700

    [ux-001] made user experience enhancements for header, home page, and recipes page

--- src/components/header/HeaderLinksContainer.jsx
+++ src/components/header/HeaderLinksContainer.jsx
@@ -4,6 +4,9 @@ const HeaderLinksContainer = () =>
   <div className="header-links">
     <HeaderLink path={'/'} classname={'header-link'} label={'home'} />
     <HeaderLink path={"/recipes"} classname={"header-link"} label={"recipes"} />
+    <HeaderLink path={"/recipes"} classname={"header-link"} label={"my recipes"} />
+    <HeaderLink path={"/ingredients"} classname={"header-link"} label={"my pantry"} />
+    <HeaderLink path={"/login"} classname={"header-link"} label={"log in"} />
   </div>
 
 export default HeaderLinksContainer;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,44 @@
 import React from "react";
+import homePageConfig from "../assets/configs/home.json";
+import section1Img from "../assets/images/rec-app-home-section-1.jpg";
+import section2Img from "../assets/images/rec-app-home-section-2.jpg"
+import "./page.css";
 
 const HomePage = () =>
-  <p>HomePage works!</p>
+  <div className="page-content">
+    <section id="home-page-hero" className="page-section">
+      <h1 className="home-page-header">Welcome to Recipe Rollout!</h1>
+      <p className="hero-text">Unleash a world of flavor with just a tap - discover an endless array of delectable recipes on Recipe Rollout and turn every meal into a gourmet experience!</p>
+      <p className="hero-text">Be the star of the kitchen - share your culinary masterpiece with the world on Recipe Rollout and inspire fellow food lovers!</p>
+      <p className="hero-text">Get ready to turn your virtual recipe collection into a stunning, personalized cookbook with our upcoming feature on Recipe Rollout - preserving your culinary legacy in style!</p>
+    </section>
+    <section id="home-page-ad-1" className="page-section">
+      <h2>Thank you for choosing Recipe Rollout!</h2>
+      <img src={section1Img} alt="recipe-rollout-img" className="home-page-sec-img float-left space-buffer-right" />
+      <p className="section-ad-content">Welcome to Recipe Rollout - the ultimate recipes app that pusts you in the chef's seat! Elevate your culinary prowess by not only exploring a vast array of tried-and-true recipes but also by sharing your very own kitchen masterpieces with a global community of food enthusiasts. With Recipe Rollout, your creativity knows no bounds. Easily add and customize your recipes, accompanied by mouthwatering photos that showcase your culinary finesse. Become part of a dynamic food-loving family, where sharing is caring and innovation knows no limits. Whether it's your grandma's cherished secret recipe or your latest culinary experiment, CookBook Connect celebrates your unique flavors and invites you to savor the joy of cooking together. Get ready to dive into a world of taste, inspiration, and connection - join us now and let your recipes shine!</p>
+    </section>
+    <section id="home-page-ad-2" className="page-section">
+      <h2>Connect with our community of food enthusiasts!</h2>
+      <img src={section2Img} alt="recipe-rollout-img" className="home-page-sec-img float-right space-buffer-left" />
+      <p className="section-ad-content">Ignite your passion for cooking and connect with a vibrant community of fellow recipe enthusiasts on Recipe Rollout! Join us to savor the joy of cooking in the company of kindred spirits who understand that the best recipes are the ones shared among friends.</p>
+    </section>
+    <section id="home-page-ad-3" className="page-section">
+      <h2>Utilize our amazing features!</h2>
+      <div className="flex-container">
+        <article className="section-ad-content home-article">
+          <h3>Share your recipes</h3>
+          <p>Share your culinary experiences with other users on Recipe Rollout and inspire fellow food lovers!</p>
+        </article>
+        <article className="section-ad-content home-article">
+          <h3>View our recipe collection</h3>
+          <p>Need quick meal ideas? Take a look at other user recipes and whip up a satisfying, memorable meal!</p>
+        </article>
+        <article className="section-ad-content home-article">
+          <h3>Coming soon - Cookbook</h3>
+          <p>We are excited to announce an upcoming new feature: Cookbook. Stay tuned for more details.</p>
+        </article>
+      </div>
+    </section>
+  </div>
 
 export default HomePage;
