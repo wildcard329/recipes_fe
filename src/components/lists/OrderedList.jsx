2f8f4c8b6e6ec2d7acfa78960dd60ac96125b257
wildcard329 <enkiduwildman@gmail.com>
2023-08-25 16:51:55 -0700

    [ux-005] addressed ux bug on recipe viewer

--- src/components/lists/OrderedList.jsx
+++ src/components/lists/OrderedList.jsx
@@ -1,7 +1,9 @@
+import "./list.css";
+
 const UnorderedList = ({ title, data }) => 
   <div>
     <h3>{title}</h3>
-    <ol className="highlight-line">
+    <ol className="highlight-line ordered-list">
       {data?.map((item) => <li>{item}</li>)}
     </ol>
   </div>
