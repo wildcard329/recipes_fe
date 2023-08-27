b3e7bbd373252baa9cccd718e5eccc8fbcc898d2
wildcard329 <enkiduwildman@gmail.com>
2023-08-26 19:08:03 -0700

    [ux-006] added a loader and fixed bug in header

--- src/pages/RecipesPage.jsx
+++ src/pages/RecipesPage.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { API } from "aws-amplify";
 import { RecipeCard } from "../components/recipe";
+import { useBool } from "../utils/customhooks";
+import AppLoader from "../components/loader/AppLoader";
 
 const RecipesPage = () => {
   const [recipes, setRecipes] = useState([]);
+  const {
+    isTruthy: isLoading,
+    setTruthy: setIsLoading,
+    setNotTruthy: setIsNotLoading,
+  } = useBool();
 
   const getRecipes = async () => {
+    setIsLoading();
     const { data: recipes } = await API.get('recipes', '/recipes');
     await setRecipes(recipes);
+    setIsNotLoading();
   };
 
   useEffect(() => {
@@ -15,12 +24,18 @@ const RecipesPage = () => {
   }, []);
 
   return (
-    <div className="recipes-container">
-      <h1>Browse Recipes</h1>
-      <section id="recipes-section">
-        {recipes?.map((recipe) => <RecipeCard recipe={recipe} />)}
-      </section>
-    </div>
+    <>
+      {isLoading ?
+        <AppLoader />
+      :
+        <div className="recipes-container">
+          <h1>Browse Recipes</h1>
+          <section id="recipes-section">
+            {recipes?.map((recipe) => <RecipeCard recipe={recipe} />)}
+          </section>
+        </div>
+      }
+    </>
   )
 }
 
