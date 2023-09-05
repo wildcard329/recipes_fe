00071e59c381d29863f6f5fdc2356715a68a988f
wildcard329 <enkiduwildman@gmail.com>
2023-09-05 10:29:12 -0700

    [ux-015] added new logo assets and small changes to mobile header

--- src/components/logo/AppLogo.jsx
+++ src/components/logo/AppLogo.jsx
@@ -1,6 +1,27 @@
 import logo from "../../assets/images/recipe-rollout-logo.jpg";
+import logo2 from "../../assets/images/logo_asset_2.png";
+import logo3 from "../../assets/images/logo_asset_3.png";
+import "./AppLogo.css";
+import { useBool } from "../../utils/customhooks";
 
-const AppLogo = () => 
-  <img src={logo} alt="recipe-rollout-logo" height={30} width={75} />
+const AppLogo = () => {
+  const {
+    isTruthy: isFullLogo,
+    setTruthy: setIsFullLogo,
+    setNotTruthy: setNotIsFullLogo,
+  } = useBool();
+
+  return(
+    <>
+    {isFullLogo ?
+      <div onClick={setNotIsFullLogo} onMouseLeave={setNotIsFullLogo} className="logo-long-container">
+        <img src={logo2} alt="recipe-rollout-logo-2" height={31} width={137} className="logo-long" />
+        <img src={logo3} alt="recipe-rollout-logo-3" height={31} width={137} className="logo-long" />
+      </div>
+    :
+      <img src={logo} alt="recipe-rollout-logo" height={30} width={75} onMouseEnter={setIsFullLogo} />}
+    </>
+  )
+}
 
 export default AppLogo;
