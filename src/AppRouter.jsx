027244e2bfb04ccd0957a1d28a16ceaed7762bc7
wildcard329 <enkiduwildman@gmail.com>
2023-09-07 21:55:01 -0700

    [auth-001] added google authentication

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
+const AuthPage = lazy(() => import("../src/pages/AuthPage.jsx"));
 const HomePage = lazy(() => import("../src/pages/HomePage.jsx"));
 const RecipesPage = lazy(() => import("../src/pages/RecipesPage.jsx"));
 const RecipeViewerPage = lazy(() => import("../src/pages/RecipeViewerPage.jsx"));
@@ -9,6 +10,7 @@ const RecipeEditorPage = lazy(() => import("../src/pages/RecipeEditorPage.jsx"))
 const AppRouter = () =>
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/login" element={<AuthPage />} />
     <Route path="/recipes" element={<RecipesPage />} />
     <Route path="/recipe/:id" element={<RecipeViewerPage />} />
     <Route path="/recipes/new" element={<RecipeEditorPage />} />
