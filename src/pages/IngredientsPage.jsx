0f4feecef47b3239aaa721e35f7078dcc5cf4f6f
wildcard329 <enkiduwildman@gmail.com>
2023-09-23 16:25:34 -0700

    [ing-001] added content for ingredients

--- src/pages/IngredientsPage.jsx
+++ src/pages/IngredientsPage.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import ingredientsJson from "../assets/configs/ingredients.json";
 import "./IngredientsPage.css";
 
-const IngredientsPage = () => 
-  <div>
-    There will be content here.
-  </div>
+const IngredientsPage = () => {
+  const [selected, setSelected] = useState({});
+
+  const selectItem = (ingredient) => {
+    console.log('data ', ingredient);
+    setSelected(ingredient);
+  }
+  
+  return(
+      <div className="ingredients-page-wrapper">
+        <ul className="ingredients-list">
+          {ingredientsJson.map((ingredient) => <li className="ingredients-list-item" onClick={() => selectItem(ingredient)} key={ingredient.ingredient_id}><button>{ingredient.ingredient_name}</button></li>)}
+        </ul>
+        <div className="ingredients-data">
+          {!!selected && <h3>{selected.ingredient_name}</h3>}
+          {!!selected && <p>{selected.ingredient_description}</p>}
+        </div>
+      </div>
+  )
+}
 
 export default IngredientsPage;
